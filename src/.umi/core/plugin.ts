d579392258c55a1b6300abdeea3e3d609b2cab25
zmzimpl <461045745@qq.com>
2020-10-30 01:07:44 +0800

    remove .umi

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'F:/Project-React/charts-designer/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','locale','locale','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'F:/Project-React/charts-designer/node_modules/react-helmet';
