4f3aed6df1ae0e3922469b9f1471b37ecb0b2fba
zmzimpl <461045745@qq.com>
2020-12-10 01:15:54 +0800

    test gridstack

--- src/pages/designer/grid-canvas/GridCanvas.tsx
+++ src/pages/designer/grid-canvas/GridCanvas.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { fabric } from 'fabric';
-
+// import { fabric } from 'fabric';
+import 'gridstack/dist/gridstack.min.css';
+import {GridStack} from 'gridstack';
+// THEN to get HTML5 drag&drop
+// OR to get legacy jquery-ui drag&drop
+import 'gridstack/dist/jq/gridstack-dd-jqueryui';
 import styles from './GridCanvas.less';
 
 export class GridCanvas extends React.Component {
-  gridCanvas!: fabric.Canvas;
+  grid!: GridStack;
 
   componentDidMount() {
     this.initCanvas();
@@ -19,42 +23,47 @@ export class GridCanvas extends React.Component {
   }
 
   initCanvas() {
-    const container: HTMLDivElement = document.getElementById('grid-container') as HTMLDivElement;
-    const canvas = new fabric.Canvas('grid-canvas', {
-        selection: false,
-        height: container.clientHeight,
-        width: container.clientWidth,
+    // const container: HTMLDivElement = document.getElementById('grid-container') as HTMLDivElement;
+    // const canvas = new fabric.Canvas('grid-canvas', {
+    //     selection: false,
+    //     height: container.clientHeight,
+    //     width: container.clientWidth,
+    // });
+    this.grid = GridStack.init({
+      resizable: {
+        handles: 'e, se, s, sw, w'
+      }
     });
-    this.gridCanvas = canvas;
+    this.grid.addWidget({x: 2, width: 10, height: 10 , content: 'item 1'});
   }
 
   drawGrid() {
-    const container: HTMLDivElement = document.getElementById('grid-container') as HTMLDivElement;
-    const options = {
-        distance: 10,
-        width: container.clientWidth,
-        height: container.clientHeight,
-        param: {
-          stroke: '#ebebeb',
-          strokeWidth: 1,
-          selectable: false,
-        },
-      };
-      if (options) {
-        const gridLen = options.width / options.distance;
+    // const container: HTMLDivElement = document.getElementById('grid-container') as HTMLDivElement;
+    // const options = {
+    //     distance: 10,
+    //     width: container.clientWidth,
+    //     height: container.clientHeight,
+    //     param: {
+    //       stroke: '#ebebeb',
+    //       strokeWidth: 1,
+    //       selectable: false,
+    //     },
+    //   };
+    //   if (options) {
+    //     const gridLen = options.width / options.distance;
   
-        for (var i = 0; i < gridLen; i++) {
-          const distance = i * options.distance,
-            horizontal = new fabric.Line([distance, 0, distance, options.width], options.param),
-            vertical = new fabric.Line([0, distance, options.width, distance], options.param);
-          this.gridCanvas.add(horizontal);
-          this.gridCanvas.add(vertical);
-          if (i % 5 === 0) {
-            horizontal.set({ stroke: '#cccccc' });
-            vertical.set({ stroke: '#cccccc' });
-          }
-        }
-      }
+    //     for (var i = 0; i < gridLen; i++) {
+    //       const distance = i * options.distance,
+    //         horizontal = new fabric.Line([distance, 0, distance, options.width], options.param),
+    //         vertical = new fabric.Line([0, distance, options.width, distance], options.param);
+    //       this.gridCanvas.add(horizontal);
+    //       this.gridCanvas.add(vertical);
+    //       if (i % 5 === 0) {
+    //         horizontal.set({ stroke: '#cccccc' });
+    //         vertical.set({ stroke: '#cccccc' });
+    //       }
+    //     }
+    //   }
   }
 
   drawWidget() {
@@ -68,26 +77,26 @@ export class GridCanvas extends React.Component {
     //     originY: 'top',
     //     centeredRotation: true
     // }));
-    this.gridCanvas.add(new fabric.Circle({
-        left: 300,
-        top: 300,
-        radius: 50,
-        fill: '#9f9',
-        originX: 'left',
-        originY: 'top',
-        centeredRotation: true
-    }));
+    // this.gridCanvas.add(new fabric.Circle({
+    //     left: 300,
+    //     top: 300,
+    //     radius: 50,
+    //     fill: '#9f9',
+    //     originX: 'left',
+    //     originY: 'top',
+    //     centeredRotation: true
+    // }));
   }
 
   initListener() {
-    this.gridCanvas.on('object:moving', (options) => {
-        if (options?.target) {
-            options.target.set({
-                left: Math.round((options.target.left || 0) / 10) * 10,
-                top: Math.round((options.target.top || 0) / 10) * 10
-            });
-        }
-    });
+    // this.gridCanvas.on('object:moving', (options) => {
+    //     if (options?.target) {
+    //         options.target.set({
+    //             left: Math.round((options.target.left || 0) / 10) * 10,
+    //             top: Math.round((options.target.top || 0) / 10) * 10
+    //         });
+    //     }
+    // });
   }
 
   resizeCanvas() {
@@ -99,7 +108,7 @@ export class GridCanvas extends React.Component {
   render() {
     return (
       <div className={styles.canvasContainer} id="grid-container">
-        <canvas id="grid-canvas" className={styles.canvasWrapper}></canvas>
+        <div className="grid-stack"></div>
       </div>
     );
   }
